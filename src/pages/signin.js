b58eaf0a1c7216fdc03376d7ce18310436570c63
Yajurva Trivedi <yajurva.trivedi@ryerson.ca>
2024-01-26 01:12:52 -0500

    completed frontend for signup-signin

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,85 +1,126 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import './signin.css';
-import { useUser } from '../UserSession'
+import { useUser } from '../UserSession';
+import signin from '../images/signin.svg' ;
+import Navbar from './components/nav';
+import { Button } from './components/button/Button';
+
 
 export default function Signin() {
-    const [email, setemail] = useState('');
-    const [password, setpassword] = useState('');
-    let currUserId = "";
-    const {setLoggedUser, setAdminStatus} = useUser();
-    const navigate = useNavigate();
-    /**
-     * 
-     * @param {JSON} userInfo User info in JSON format
-     * @returns User Id if success / false if fail
-     */
-    const fetchId = async (userInfo) => {
-      try {
-        const resp = await fetch("http://localhost:5000/signin", {
-          method: "post",
-          body: JSON.stringify(userInfo),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-    
-        if (resp.ok) {
-          const userId = await resp.json();
-          return userId;
-        } else {
-          return false;
-        }
-      } catch (error) {
-        console.log(error);
-        return null;
-      }
-    };
-  
-    
-    /**
-     * Handles Submit Form
-     * @param {Event} event Form Data 
-     */
-    const submitform = async event => { 
-      event.preventDefault();
-      // Checking is user entry is valid
-      
-      const userInfo = {
-        email : email, 
-        password : password
-      }; 
-      
-      // Fetchig new User ID
-      currUserId = await fetchId(userInfo);
-      if (currUserId) {
-        setLoggedUser(currUserId);
-        if (email == "admin@gmail.com") {
-          setAdminStatus(true);
-          navigate("/admin");
-          return;
-        }
-        console.log(`User has successfully logged in: ${currUserId}`);
-        navigate("/dashboard");
-      }
-      else {
-        alert("Incorrect User Information");
-      }
-         
-    }
+  const [email, setemail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
-    return(<>
-        <div>Sign In</div>
-        <div className="signin" id = "signin">
-            <h2>Welcome back </h2>
-                <div className = "signin-fields">
-                <input type = "Email" placeholder="Email" required value={email} onChange={(e) => setemail(e.target.value)}/>
-                <input type = "password" placeholder="Password" required value={password} onChange={(e) => setpassword(e.target.value)}/>
+  let currUserId = "";
+  const {setLoggedUser, setAdminStatus} = useUser();
+  const navigate = useNavigate();
+  /**
+   * 
+   * @param {JSON} userInfo User info in JSON format
+   * @returns User Id if success / false if fail
+   */
+  const fetchId = async (userInfo) => {
+    try {
+      const resp = await fetch("http://localhost:5000/signin", {
+        method: "post",
+        body: JSON.stringify(userInfo),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+  
+      if (resp.ok) {
+        const userId = await resp.json();
+        return userId;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  };
+
+  
+  /**
+   * Handles Submit Form
+   * @param {Event} event Form Data 
+   */
+  const submitform = async event => { 
+    event.preventDefault();
+    // Checking is user entry is valid
+    
+    const userInfo = {
+      email : email, 
+      password : password
+    }; 
+    
+    // Fetchig new User ID
+    currUserId = await fetchId(userInfo);
+    if (currUserId) {
+      setLoggedUser(currUserId);
+      if (email == "admin@gmail.com") {
+        setAdminStatus(true);
+        navigate("/admin");
+        return;
+      }
+      console.log(`User has successfully logged in: ${currUserId}`);
+      navigate("/dashboard");
+    }
+    else {
+      alert("Incorrect User Information");
+    }
+        
+  }
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return(
+    <>  
+      <Navbar/>
+      <div className="signin" id = "signin">
+        <div className='signin-form'>
+          <h2 className='sign-subheader'>Welcome back </h2>
+          <h1 className='sign-header'>Sign In</h1>
+          <div className = "signin-fields">
+            <input className='input-field' type = "email" placeholder="Email" required value={email} onChange={(e) => setemail(e.target.value)}/>          
+            <div>
+              {showPassword ? (
+                <input
+                  className='input-field'
+                  type='text'
+                  placeholder='Password'
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              ) : (
+                <input
+                  className='input-field'
+                  type='password'
+                  placeholder='Password'
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              )}
+              <button className='showPass' type='button' onClick={togglePasswordVisibility}>
+                {showPassword ? 'Hide Password' : 'Show Password'}
+              </button>
             </div>
-            <button onClick={submitform}>
-                Login
-            </button>
-            <p>Don't have an account? <a href="./signup"><b>Create one</b></a></p>
+          </div>
+          <Button buttonColor='primary' buttonSize='btn-medium' buttonStyle='btn-primary'onClick={submitform}>
+              Login
+          </Button>
+          <p className='signup-prompt'>Don't have an account? <a href="./signup"><b><span className='different-color'>Create one</span></b></a></p>
         </div>
-    </>)
+        <div className='signin-img'>
+          <img className='signinpic' src={signin} alt=""/>
+        </div>
+      </div>
+    </>
+  )
 }
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,134 +1,144 @@
 import React, { useState } from 'react'
 import './signup.css'
 import { useNavigate } from 'react-router-dom';
+import { Button } from './components/button/Button';
+import signin from '../images/signin.svg' ;
+import Navbar from './components/nav';
 
 export default function Signup() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    // Setting user, email, password, cpassword states
-    const [user, setuser] = useState('');
-    const [email, setemail] = useState('');
-    const [password, setpassword] = useState('');
-    const [cpassword, setcpassword] = useState('');
-    let currUserId = "";
-  
-    // Keeping track of email/pass validity
-    let emailIsValid = false;
-    let passlIsValid = false;
-  
-  
-    /**
-     * Checks if the user input email is valid or not
-     */
-    const emailValidation = () => {
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      const isValidEmail = emailRegex.test(email);
-      if (isValidEmail) {
-        emailIsValid = true;
-      }
-      else {
-        emailIsValid = false;
-      }
+  // Setting user, email, password, cpassword states
+  const [user, setuser] = useState('');
+  const [email, setemail] = useState('');
+  const [password, setpassword] = useState('');
+  const [cpassword, setcpassword] = useState('');
+  let currUserId = "";
+
+  // Keeping track of email/pass validity
+  let emailIsValid = false;
+  let passlIsValid = false;
+
+
+  /**
+   * Checks if the user input email is valid or not
+   */
+  const emailValidation = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValidEmail = emailRegex.test(email);
+    if (isValidEmail) {
+      emailIsValid = true;
     }
-  
-    /**
-     * Compares user input pass with compare pass
-     */
-    const passValidation = () => {    
-      if ((password.length > 8) && (!cpassword.localeCompare(password)))  {
-        passlIsValid = true;
-      }
-      else {
-        passlIsValid = false;   
-      }
+    else {
+      emailIsValid = false;
     }
-  
-    /**
-     * 
-     * @param {JSON} userInfo User info in JSON format
-     * @returns User Id if success / false if fail
-     */
-    const fetchId = async (userInfo) => {
-      try {
-        const resp = await fetch("http://localhost:5000/signup", {
-          method: "post",
-          body: JSON.stringify(userInfo),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-    
-        if (resp.ok) {
-          const userId = await resp.json();
-          return userId;
-        } else {
-          return false;
-        }
-      } catch (error) {
-        console.log(error);
-        return null;
-      }
-    };
-  
-    
-    /**
-     * Handles Submit Form
-     * @param {Event} event Form Data 
-     */
-    const submitform = async event => { 
-      event.preventDefault();
-  
-      emailValidation();
-      passValidation();
-  
-      // Checking is user entry is valid
-      if (emailIsValid && passlIsValid) {      
-        const userInfo = {
-          name : user, 
-          email : email, 
-          password : password
-        }; 
-        
-        // Fetchig new User ID
-        currUserId = await fetchId(userInfo);
-        if (currUserId) {
-          alert(`User has been successfully added. The User Id is : ${currUserId}`);
-        }
-        else {
-          alert("Sign up failed");
-        }
-      }    
-      else {      
-        if (!(emailIsValid || passlIsValid)) {
-          console.log(emailIsValid, passlIsValid);
-          alert("Your email and password is invalid");
-        }
-        else if (!emailIsValid) {
-          alert("Your email is invalid");
-        }
-        else {
-          alert("Your password is invalid");
-        }
-      }
-      navigate('/signin');
-    }
-      
-    return (
-    <>
-        <div className="signup" id = "signup">
-            <h2> Sign up</h2>
-                <div className = "signup-fields">
-                <form>
-                  <input type = "textbox" placeholder="Name"  required value={user} onChange={(e) => setuser(e.target.value)}/>
-                  <input type = "Email" placeholder="Email" required value={email} onChange={(e) => setemail(e.target.value)}/>
-                  <input type = "password" placeholder="Password" required value={password} onChange={(e) => setpassword(e.target.value)}/>
-                  <input type = "password" placeholder="Confirm your password" required value={cpassword} onChange={(e) => setcpassword(e.target.value)}/>
-                </form>
-            </div>
-            <button onClick={submitform}>
-              Sign up
-            </button>
-        </div>
-    </>
-    )
   }
+
+  /**
+   * Compares user input pass with compare pass
+   */
+  const passValidation = () => {    
+    if ((password.length > 8) && (!cpassword.localeCompare(password)))  {
+      passlIsValid = true;
+    }
+    else {
+      passlIsValid = false;   
+    }
+  }
+
+  /**
+   * 
+   * @param {JSON} userInfo User info in JSON format
+   * @returns User Id if success / false if fail
+   */
+  const fetchId = async (userInfo) => {
+    try {
+      const resp = await fetch("http://localhost:5000/signup", {
+        method: "post",
+        body: JSON.stringify(userInfo),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+  
+      if (resp.ok) {
+        const userId = await resp.json();
+        return userId;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  };
+
+  
+  /**
+   * Handles Submit Form
+   * @param {Event} event Form Data 
+   */
+  const submitform = async event => { 
+    event.preventDefault();
+
+    emailValidation();
+    passValidation();
+
+    // Checking is user entry is valid
+    if (emailIsValid && passlIsValid) {      
+      const userInfo = {
+        name : user, 
+        email : email, 
+        password : password
+      }; 
+      
+      // Fetchig new User ID
+      currUserId = await fetchId(userInfo);
+      if (currUserId) {
+        alert(`User has been successfully added. The User Id is : ${currUserId}`);
+      }
+      else {
+        alert("Sign up failed");
+      }
+    }    
+    else {      
+      if (!(emailIsValid || passlIsValid)) {
+        console.log(emailIsValid, passlIsValid);
+        alert("Your email and password is invalid");
+      }
+      else if (!emailIsValid) {
+        alert("Your email is invalid");
+      }
+      else {
+        alert("Your password is invalid");
+      }
+    }
+    navigate('/signin');
+  }
+      
+  return (
+  <>
+    <Navbar/>
+    <div className="signup" id = "signup">
+      <div className='signup-form'>
+        <h2 className='sign-subheader'>Hi there!</h2>
+        <h1 className='sign-header'>Sign Up</h1>
+        <div className = "signin-fields">
+          <form className='signup-form-form'>
+            <input className='input-field' type = "textbox" placeholder="Name"  required value={user} onChange={(e) => setuser(e.target.value)}/>    
+            <input className='input-field' type = "Email" placeholder="Email" required value={email} onChange={(e) => setemail(e.target.value)}/>
+            <input className='input-field' type = "password" placeholder="Password" required value={password} onChange={(e) => setpassword(e.target.value)}/>
+            <input className='input-field' type = "password" placeholder="Confirm your password" required value={cpassword} onChange={(e) => setcpassword(e.target.value)}/>        
+          </form>
+          <Button buttonColor='primary' buttonSize='btn-medium' buttonStyle='btn-primary' onClick={submitform}>
+            Sign up
+          </Button>
+        </div>
+      </div>
+      <div className='signin-img'>
+        <img className='signinpic' src={signin} alt=""/>
+      </div>
+    </div>
+  </>
+  )
+}
